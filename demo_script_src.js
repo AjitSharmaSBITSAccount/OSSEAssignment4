e640dae39a73526a61eb387ea9a7805399677f0d
Rasagnya Gurrala <rasagnya.gurrala@sap.com>
2021-12-18 21:37:52 +0530

    added navigation code

--- demo_script_src.js
+++ demo_script_src.js
@@ -1,8 +1,8 @@
 function myFunction() {
     // location.replace("../resume-mini-project/type2Resume.html");
-    window.location.href="template3.html";
+    window.location.href="template2.html";
 }
 function myFunction2() {
     // location.replace("../resume-mini-project/type2Resume.html");
-    window.location.href="template2.html";
+    window.location.href="template3.html";
 }
